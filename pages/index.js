b6a44442fd5c5e049eb57752e8c792b88cf1734a
William Hunter <30416118+wlhunter00@users.noreply.github.com>
2022-12-20 20:13:23 -0500

    created repo

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Container from '@mui/material/Container';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Present Protocol</title>
+      </Head>
+      <div>
+        <Container>
+          <p>Test</p>
+        </Container>
+      </div>
+    </>
+  )
+}
