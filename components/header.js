09b6dfe20ac77f122f91314a49867a900c18041d
William Hunter <30416118+wlhunter00@users.noreply.github.com>
2022-12-20 20:56:16 -0500

    installed dynamic

---
+++ components/header.js
@@ -0,0 +1,9 @@
+import { DynamicWidget } from '@dynamic-labs/sdk-react';
+
+export function Header() {
+    return (
+        <div>
+            <DynamicWidget />
+        </div>
+    )
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,40 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { DynamicContextProvider } from '@dynamic-labs/sdk-react';
+import { ethers } from "ethers";
+import Head from "next/head";
+import { Header } from "../components/header";
+
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // TODO Change for mainnet
+  const defaultProvider = new ethers.providers.AlchemyProvider(
+    "goerli",
+    process.env.NEXT_PUBLIC_ALCHEMY_API_KEY_GOERLI
+  );
+
+  return (
+    <DynamicContextProvider
+      settings={{
+        appLogoUrl:
+          "https://upload.wikimedia.org/wikipedia/commons/3/34/Examplelogo.svg",
+        appName: "Present Protocol",
+        environmentId: "db7a8968-4e63-4ccd-bf6b-c7082545ee22",
+        // privacyPolicyUrl: "/privacypolicy",
+        // termsOfServiceUrl: "/termsofservice",
+        onAuthSuccess: ({ authToken, user }) => {
+          console.log(
+            `Welcome ${user.walletPublicKey} your token expires on ${authToken.exp}`
+          );
+          window.location.assign("/");
+        },
+      }}
+    >
+      <Head>
+        <title>Present Protocol</title>
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+    </DynamicContextProvider>
+  );
 }
